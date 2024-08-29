8592839b0bdae3e0a973cccb823b5c5ad185bfde
Zhang Hua <zhanghua.00@qq.com>
2024-08-29 20:35:02 +0800

    Remove QQ files

--- patch/preload_simple.js
+++
@@ -1,4 +0,0 @@
-{try{ const { contextBridge } = require('electron');
-contextBridge.exposeInMainWorld('electron',{load: (file) => { require('../major.node').load(file, module);}});
-}catch{}
-require('../major.node').load('p_preload_simple', module);}
